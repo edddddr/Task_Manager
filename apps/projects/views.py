6dd8fc08dd2ed7b3ab8e57abb46be45e76f3161c
edddddr <endt6342@gmail.com>
2025-12-20 23:47:45 -0900

    ref: fat model and thin views

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -10,47 +10,35 @@ from apps.users.models import User
 @ajax_login_required
 @csrf_exempt 
 def project_list_create(request):
+
     if request.method == "GET":
         projects = Project.objects.all()
-        data = [{"status": "success"}]
-        for project in projects:
-            data.append({
-                    "id": project.id,
-                    "name": project.name,
-                    "description": project.description,
-                    "created_by": project.created_by.id if project.created_by else None,
-                    "members": [user.id for user in project.members.all()],
-                    "created_at": project.created_at,
-                    "updated_at": project.updated_at
-            })
+        data = [project.to_dict() for project in projects] # List a projects wiht custome an instance method
         return JsonResponse(data, safe=False)
-
+    
     elif request.method == "POST":
         try:
             body = json.loads(request.body)
-            project = Project.objects.create(
+
+            project = Project.create_project(
                 name=body.get("name"),
                 description=body.get("description", ""),
-                created_by=request.user
-            )
-            return JsonResponse({
-                "status": "success",
-                "project" : {
-                    "id": project.id,
-                    "name": project.name,
-                    "description": project.description,
-                    "created_by": project.created_by.id,
-                    "members": [],
-                    "created_at": project.created_at,
-                    "updated_at": project.updated_at
-                    }
-                }, 
+                creator=request.user
+            ) # Create a project with custome class based method
+
+            return JsonResponse(
+                {"status": "success", "project": project.to_dict()},
                 status=201
             )
+
         except Exception as e:
-            return JsonResponse({"status" : "error", "message": str(e)}, status=400)
-    else:
-        return HttpResponseNotAllowed(["GET", "POST"])
+            return JsonResponse(
+                {"status": "error", "message": str(e)},
+                status=400
+            )
+
+    return HttpResponseNotAllowed(["GET", "POST"])
+
 
 
 # Retrieve, Update, Delete Project
@@ -60,35 +48,20 @@ def project_detail(request, project_id):
     project = get_object_or_404(Project, id=project_id)
 
     if request.method == "GET":
-        data = {
-            "id": project.id,
-            "name": project.name,
-            "description": project.description,
-            "created_by": project.created_by.id if project.created_by else None,
-            "members": [user.id for user in project.members.all()],
-            "created_at": project.created_at,
-            "updated_at": project.updated_at,
-        }
-        return JsonResponse(data)
+        return JsonResponse(project.to_dict()) #List a project wiht custome instance method
 
-    elif request.method == "PUT" or request.method == "PATCH":
+
+    elif request.method in ["PUT", "PATCH"]:
         try:
             body = json.loads(request.body)
-            project.name = body.get("name", project.name)
-            project.description = body.get("description", project.description)
-            project.save()
-            return JsonResponse({
-                "status" : "success",
-                "project" :{
-                    "id": project.id,
-                    "name": project.name,
-                    "description": project.description,
-                    "created_by": project.created_by.id if project.created_by else None,
-                    "members": [user.id for user in project.members.all()],
-                    "created_at": project.created_at,
-                    "updated_at": project.updated_at
-                    }
-            }, status=200)
+            project.update_project(
+                name=body.get("name"),
+                description=body.get("description")
+            )
+            return JsonResponse(
+                {"status": "success", "project": project.to_dict()},
+                status=200
+            )
         except Exception as e:
             return JsonResponse({"status" : "error", "message": str(e)}, status=400)
 
