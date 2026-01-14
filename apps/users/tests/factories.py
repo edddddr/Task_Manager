07b80e90c8f5ce4b74d8de07e3b5a7b9004be997
edddddr <endt6342@gmail.com>
2026-01-14 04:49:23 -0900

    reaf

--- apps/users/tests/factories.py
+++ apps/users/tests/factories.py
@@ -12,7 +12,6 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
         django_get_or_create = ()
-        skip_postgeneration_save = True 
 
     username = factory.Sequence(lambda n: f"user{n}")
     email = factory.Sequence(lambda n: f"user{n}@example.com")
